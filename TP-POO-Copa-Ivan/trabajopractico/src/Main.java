8ce31ed7e088bc90f3be60ff085c796db92c8ac9
Iván Jesús <ivan.copa@davinci.edu.ar>
2021-12-05 22:05:07 -0300

    primer commit

---
+++ TP-POO-Copa-Ivan/trabajopractico/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria("Copa Iván", "42897007");
+        cuenta1.mostrarMenu();
+    }
+}
